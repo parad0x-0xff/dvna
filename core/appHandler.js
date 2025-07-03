1f703474e5633a8f49cd0034a797ce2313a3a4f1
Allan Trindad <at.ferreira@protonmail.com>
2025-07-03 17:48:33 -0300

    core app update

--- core/appHandler.js
+++ core/appHandler.js
@@ -6,6 +6,7 @@ var libxmljs = require("libxmljs");
 var serialize = require("node-serialize")
 const Op = db.Sequelize.Op
 
+
 module.exports.userSearch = function (req, res) {
 	var query = "SELECT name,id FROM Users WHERE login='" + req.body.login + "'";
 	db.sequelize.query(query, {
